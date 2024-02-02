27a1db6b11e45f09a11229751802f656734eeade
santiperez98 <santip19982022@gmail.com>
2024-02-02 16:06:18 -0300

    vaquedando

--- src/pages/productos.js
+++ src/pages/productos.js
@@ -2,22 +2,21 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Landing from '@/components/Landing/Landing'
 import Cards from '@/components/Cards/Cards'
-import NavBar from '@/components/NavBar/Navbar'
+import NavBar from '@/components/NavBar'
 import Prueba from '@/components/Prueba/Prueba'
-import Carousel from '../components/Carousel/Carousel'
-import { Footer } from '@/components/Footer/Footer'
+
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Productos() {
   return (
     <div> 
+   
+   <Prueba/>
   
-  <NavBar/>
   <Landing/>
-  <Carousel/>
+  
 
   <Cards/>
-  <Footer/>
   </div>
   )
 }
