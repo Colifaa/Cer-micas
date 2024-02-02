841180f0a9358f638948d1e9af1e2dafa71bf016
unknown <Jorgemathez14@gmail.com>
2024-02-02 17:45:20 -0300

    aver

--- src/pages/productos.js
+++ src/pages/productos.js
@@ -2,14 +2,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Landing from '@/components/Landing/Landing'
 import Cards from '@/components/Cards/Cards'
-import Prueba from '@/components/Prueba/Prueba'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Productos() {
   return (
     <div> 
-   <Prueba/>
+  
   <Landing/>
 
   </div>
