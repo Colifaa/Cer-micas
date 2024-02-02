b7ba6e13208a84090273f14277b7ec15f05ef726
unknown <Jorgemathez14@gmail.com>
2024-02-02 17:24:40 -0300

    bugs3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Landing from '@/components/Landing/Landing'
 import Cards from '@/components/Cards/Cards'
-import NavBar from '@/components/NavBar/Navbar'
 import Prueba from '@/components/Prueba/Prueba'
 import Carousel from '../components/Carousel/Carousel'
 import { Footer } from '@/components/Footer/Footer'
@@ -12,7 +11,7 @@ export default function Home() {
   return (
     <div> 
   
-  <NavBar/>
+
   <Landing/>
   <Carousel/>
 
