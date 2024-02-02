509632eb7fa0f1d158c3bae2cb1dd38ad9809a79
unknown <Jorgemathez14@gmail.com>
2024-02-02 17:29:42 -0300

    bugs6

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ export default function About() {
   <Landing/>
   
 
-  <Cards/>
+
   </div>
   )
 }
--- src/pages/productos.js
+++ src/pages/productos.js
@@ -11,7 +11,7 @@ export default function Productos() {
     <div> 
    <Prueba/>
   <Landing/>
-  <Cards/>
+
   </div>
   )
 }
