5a65f677c258e02911e99873af5aca7a742d6485
unknown <Jorgemathez14@gmail.com>
2024-02-02 17:18:19 -0300

    bugs2

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Landing from '@/components/Landing/Landing'
 import Cards from '@/components/Cards/Cards'
-import NavBar from '@/components/NavBar'
 import Prueba from '@/components/Prueba/Prueba'
 
 const inter = Inter({ subsets: ['latin'] })
