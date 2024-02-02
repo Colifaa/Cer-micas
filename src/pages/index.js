c91008db5ba782ad2c87f1b2c6dcadf3e1b9fa86
unknown <Jorgemathez14@gmail.com>
2024-02-02 17:56:27 -0300

    manito ?

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import Landing from '@/components/Landing/Landing'
 import Prueba from '@/components/Prueba/Prueba'
 import Carousel from '../components/Carousel/Carousel'
 import Navbar from '@/components/NavBar/Navbar'
-import  Footer from '@/components/Footer/Footer'
+import Footer from '@/components/Footer/Footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
