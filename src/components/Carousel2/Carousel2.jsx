6ad27e206fae1d2f3ac9dba6078dda08edf84cf3
unknown <Jorgemathez14@gmail.com>
2024-03-06 01:18:01 -0300

    carousel2 sin import React ?

--- src/components/Carousel2/Carousel2.jsx
+++ src/components/Carousel2/Carousel2.jsx
@@ -1,23 +1,11 @@
-import { Carousel } from "@material-tailwind/react";
- 
-export default function Carousel2() {
+import React from 'react'
+
+function Carousel2() {
   return (
-    <Carousel transition={{ duration: 2 }} className="rounded-xl">
-      <img
-        src="/images/carro1.png"
-        alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="/images/carro2.png"
-        alt="image 2"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="/images/carro3.png"
-        alt="image 3"
-        className="h-full w-full object-cover"
-      />
-    </Carousel>
-  );
+    <div>
+      aaaa
+    </div>
+  )
 }
+
+export default Carousel2
