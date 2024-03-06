521ed57fedf805b5e0dbae3a3244feb8abf7432b
unknown <Jorgemathez14@gmail.com>
2024-03-06 01:49:41 -0300

    sin scrolling

--- src/pages/ambientes.js
+++ src/pages/ambientes.js
@@ -1,13 +1,13 @@
 
-import ScrollingMenu from '@/components/ScrollingMenu/ScrollingMenu'
+
 import React from 'react'
 
 export default function Ambientes() {
   return (
     <div> 
 
-      <ScrollingMenu/>
-      
+     
+      asfasdg
 
   </div>
   )
