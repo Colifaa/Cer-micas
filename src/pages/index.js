92351334da28badf377169e488d05dcc234fa664
santiperez98 <santip19982022@gmail.com>
2024-03-06 21:17:30 -0300

    navbarvamos

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Landing from '@/components/Landing/Landing';
 import Footer from '@/components/Footer/Footer';
 import Cards from '@/components/Cards/Cards';
 import Layout from '@/components/Layout/Layout';
-
+import Navbar from '@/components/NavBar/Navbar'
 
 
 
@@ -26,7 +26,7 @@ export default function Home() {
 
         <>
        
-   
+          <Navbar/>
           <Landing />
       
           <Cards />
