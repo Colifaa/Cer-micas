2d07021d3cb7df0cb7858f608fb0e92bd5bf78b8
santiperez98 <santip19982022@gmail.com>
2024-03-08 17:45:19 -0300

    navbarsign/login

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
                 <div id="mega-menu-full" className="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-
+                            <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-
                             0 dark:text-white md:dark:hover:text-blue-
                             500 dark:hover:bg-gray-
                             700 dark:hover:text-blue-
@@ -37,7 +37,7 @@ function Navbar() {
                             gray-
                             700"
                             aria-current="
-                            page">Home</a>
+                            page">Inicio</a>
                         </li>
                         <li>
                             <button 
@@ -47,21 +47,15 @@ function Navbar() {
                                 mega-menu-full-dropdown" 
                                 onClick={toggleDropdown} 
                                 className="
-                                flex items-center justify-between w-full py-
-                                2 px-
-                                3 text-gray-
-                                900 rounded md:w-auto hover:bg-gray-
-                                100 md:hover:bg-transparent md:border-
-                                0 md:hover:text-blue-
-                                600 md:p-
-                                0 dark:text-white md:dark:hover:text-blue-
-                                500 dark:hover:bg-gray-
-                                700 dark:hover:text-blue-
-                                500 md:dark:hover:bg-transparent dark:border-
-                                gray-
-                                700"
+                                flex items-center  py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-
+                            0 dark:text-white md:dark:hover:text-blue-
+                            500 dark:hover:bg-gray-
+                            700 dark:hover:text-blue-
+                            500 md:dark:hover:bg-transparent dark:border-
+                            gray-
+                            700"
                             >
-                                Company
+                                Productos
                                 <svg className="
                                 w-
                                 2.5 h-
@@ -142,17 +136,39 @@ function Navbar() {
                         )}
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-
+                            0 dark:text-white md:dark:hover:text-blue-
+                            500 dark:hover:bg-gray-
+                            700 dark:hover:text-blue-
+                            500 md:dark:hover:bg-transparent dark:border-
+                            gray-
+                            700">Ambientes</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Resources</a>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-
+                            0 dark:text-white md:dark:hover:text-blue-
+                            500 dark:hover:bg-gray-
+                            700 dark:hover:text-blue-
+                            500 md:dark:hover:bg-transparent dark:border-
+                            gray-
+                            700">Empresa</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-
+                            0 dark:text-white md:dark:hover:text-blue-
+                            500 dark:hover:bg-gray-
+                            700 dark:hover:text-blue-
+                            500 md:dark:hover:bg-transparent dark:border-
+                            gray-
+                            700">Contacto</a>
                         </li>
                     </ul>
+                    <button href="" className="bg-white border-black rounded-lg p-2 mx-2">Sign In</button>
+                    <button href=""  className="bg-black text-white border-white rounded-lg p-2">Log In</button>
                 </div>
+                
             </div>
+
         </nav>
     );
 }
