f0747ae82284726646f5a4ab0be309c5d599768b
santiperez98 <santip19982022@gmail.com>
2024-03-27 19:41:34 -0300

    yaportermi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Landing from '@/components/Landing/Landing';
-
 import Footer from '@/components/Footer/Footer';
 import Cards from '@/components/Cards/Cards';
 import Layout from '@/components/Layout/Layout';
@@ -8,11 +7,6 @@ import Carousel from '@/components/Carousel/Carousel';
 import Navbar from '@/components/NavBar/Navbar';
 import CarouselPri from '@/components/CarouselPri/CarouselPri';
 import Marcas from '@/components/Marcas/Marcas';
-
-
-
-
-
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -25,33 +19,59 @@ export default function Home() {
 
     return () => clearTimeout(timer);
   }, []);
+
   const images = [
     "/images/1.png",
-    "/images/2.png",
-    
-   
-  ]
+    "/images/2.png",   
+  ];
 
   const images1 = [
     "/images/3.png",
-    "/images/4.png",
-    
-   
-  ]
-
+    "/images/4.png",  
+  ];
 
   return (
-
-        <>
+    <>
+      <Navbar />
+      <CarouselPri images={images} autoSlide={true} autoSlideInterval={5000} />
+      <Landing />
+      <Carousel images1={images1} autoSlide={true} autoSlideInterval={5000} />
+      <Cards />
+      <Layout />
+      <Marcas />
+      <Footer />
+      <a
+        href="https://wa.me/542604531120"
+        className="whatsapp-float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="whatsapp-icon"></div>
+      </a>
+      <style jsx>{`
+        .whatsapp-float {
+          position: fixed;
+          width: 60px;
+          height: 60px;
+          bottom: 40px;
+          right: 40px;
+          border-radius: 50%;
         
-          <Navbar/>
-          <CarouselPri images={images} autoSlide={true} autoSlideInterval={5000} />
-          <Landing />
-          <Carousel images1={images1} autoSlide={true} autoSlideInterval={5000}/>
-          <Cards />
-          <Layout/>
-          <Marcas/>
-          <Footer />
-        </>
-      )}
+          box-shadow: 2px 2px 3px #999; /* Sombra */
+          z-index: 1000;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
+        .whatsapp-icon {
+          width: 50px;
+          height: 50px;
+          background: url('/images/whatsapp.png') center center no-repeat; /* Imagen del botón */
+          background-size: cover;
+          border-radius: 50%;
+        }
+      `}</style>
+    </>
+  );
+}
